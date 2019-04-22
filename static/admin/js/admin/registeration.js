47adb4434f460290cf9a43f86e4301b2b048037d
NI <ni.sirarprapa@gmail.com>
2019-04-22 21:53:28 +0700

    Create css addboard and link auth in sign up-btn

--- static/admin/js/admin/registeration.js
+++ static/admin/js/admin/registeration.js
@@ -1,15 +1,3 @@
 $(document).ready(function(){
-  var authenticationSuccess = function(){
-    // window.location.href= "all-project.html"
-    // Trello.authorize(opts)
-    console.log('Success authentication');
-
-  };
-  var authenticationFailure = function() {
-    // window.location.href= "login.html"
-    console.log('Failed authentication');
-  };
-  $(".submit_reg").on("click",function(){
   
-  });
 });
