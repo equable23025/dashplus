602c2e4e5a30377bb67c947e7acf2c2b8b080e18
Ittigorn <Boat_z_555@hotmail.com>
2019-06-10 21:23:18 +0700

    eiei

--- project401/settings.py
+++ project401/settings.py
@@ -17,7 +17,7 @@ from django_filters.rest_framework import DjangoFilterBackend, FilterSet
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # from django_filters import rest_framework
-
+# 
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/2.0/howto/deployment/checklist/
