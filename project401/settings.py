f2a2bb5569401faa74770e37ed2f8c965b6e1dc4
Ittigorn <Boat_z_555@hotmail.com>
2019-06-10 21:51:58 +0700

    database

--- project401/settings.py
+++ project401/settings.py
@@ -95,16 +95,16 @@ WSGI_APPLICATION = 'project401.wsgi.application'
 # https://docs.djangoproject.com/en/2.0/ref/settings/#databases
 
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'trello_test',
-        'USER': 'postgres',
-        'PASSWORD' : '1234',
-        'HOST': 'localhost',
-        'PORT': 5432,
-    },
-}
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql',
+#         'NAME': 'trello_test',
+#         'USER': 'postgres',
+#         'PASSWORD' : '1234',
+#         'HOST': 'localhost',
+#         'PORT': 5432,
+#     },
+# }
 
 # Password validation
 # https://docs.djangoproject.com/en/2.0/ref/settings/#auth-password-validators
